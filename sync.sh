d514282b2480c1fad2bb6ec6cd7c61a4684699b4
plantikow@zib.de <plantikow@zib.de>
2008-07-14 14:34:33 +0200

    Added StageIn, Pin, RePublish ORQ and context types.

--- sync.sh
+++ sync.sh
@@ -5,10 +5,19 @@ for SERVICE in services/* ; do
   rsync -aurl "$SERVICE/schema/" "$TARGET/"
 done
 for SERVICE in services/* ; do
+  for gar in "$SERVICE"/*.gar ; do
+    echo ln -sf "$gar" .
+    ln -sf "$gar" .
+  done
   for jar in extra/lib/* ; do
+    echo ln -sf "../../../$jar" "$SERVICE/lib" 
     ln -sf "../../../$jar" "$SERVICE/lib" 
   done
   for jar in extra/tools-lib/* ; do
-    ln -sf "../../../../$jar" "$SERVICE/tools/lib" 
+    echo ln -sf "../../../../$jar" "$SERVICE/lib" 
+    ln -sf "../../../$jar" "$SERVICE/lib" 
   done
 done
+( cd services/DSpace && ln -sf "../../../model/lib/gndms-dspace-model.jar" lib/ )
+( cd services/LOFIS && ln -sf "../../../model/lib/gndms-lofis-model.jar" lib/ )
+( cd services/GORFX && ln -sf "../../../model/lib/gndms-gorfx-model.jar" lib/ )
