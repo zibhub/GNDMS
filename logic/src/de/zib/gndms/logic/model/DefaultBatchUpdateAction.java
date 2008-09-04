1c2780abc1d360f1c2be322fbcb8e09a6e2f48ea
plantikow@zib.de <plantikow@zib.de>
2008-09-04 15:01:45 +0200

    DefaultBatchUpdateAction ensures that batched actions are executed only once

--- logic/src/de/zib/gndms/logic/model/DefaultBatchUpdateAction.java
+++ logic/src/de/zib/gndms/logic/model/DefaultBatchUpdateAction.java
@@ -6,6 +6,7 @@ import de.zib.gndms.model.common.GridResource;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.LinkedList;
+import java.util.Collection;
 
 
 /**
@@ -44,5 +45,14 @@ public class DefaultBatchUpdateAction<M extends GridResource> extends DefaultCom
     public void addAction(final Action<Void> voidActionParam) {
         voidActionParam.setParent(this);
         super.addAction(voidActionParam);    // Overridden method
+    }
+
+
+    @Override
+    public void cleanUp() {
+        final Collection<Action<Void>> collection = getActions();
+        if (collection != null)
+            collection.clear();
+        super.cleanUp();    // Overridden method
     }
 }
