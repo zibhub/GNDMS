3357075a77d70129c02c42660a4c213ee435dbb5
plantikow@zib.de <plantikow@zib.de>
2008-11-18 18:18:02 +0100

    Fixing bug in new setup scripts, setup-dms now working!

--- scripts/features/enable-interslicetransfer.sh
+++ scripts/features/enable-interslicetransfer.sh
@@ -1,4 +1,3 @@
 enable_interslicetransfer() {
-moni call -v .gorfx.SetupOfferType "offerType: 'http://www.c3grid.de/ORQTypes/InterSliceTransfer'; orqType: '{http://gndms.zib.de/c3grid/types}InterSliceTransferORQT'; resType: '{http://gndms.zib.de/c3grid/types}InterSliceTransferResultT'; calcFactory: de.zib.gndms.GORFX.action.InterSliceTransferORQFactory; taskActionFactory: de.zib.gndms.GORFX.action.InterSliceTransferActionFactory; mod
-e:'$MODE'"
+moni call -v .gorfx.SetupOfferType "offerType:'http://www.c3grid.de/ORQTypes/InterSliceTransfer'; orqType:'{http://gndms.zib.de/c3grid/types}InterSliceTransferORQT'; resType:'{http://gndms.zib.de/c3grid/types}InterSliceTransferResultT'; calcFactory:'de.zib.gndms.GORFX.action.InterSliceTransferORQFactory'; taskActionFactory:'de.zib.gndms.GORFX.action.InterSliceTransferActionFactory'; mode:'$MODE'"
 }
