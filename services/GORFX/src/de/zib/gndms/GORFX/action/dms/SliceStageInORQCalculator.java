47583d3493972a0630951027db5f8f124bbe6ec7
mjorra@csr-priv5.zib.de <mjorra@csr-priv5.zib.de>
2008-11-18 17:21:44 +0100

    correct implementation of slice stage in (hopefully)

--- services/GORFX/src/de/zib/gndms/GORFX/action/dms/SliceStageInORQCalculator.java
+++ services/GORFX/src/de/zib/gndms/GORFX/action/dms/SliceStageInORQCalculator.java
@@ -1,17 +1,20 @@
 package de.zib.gndms.GORFX.action.dms;
 
-import de.zib.gndms.infra.system.GNDMSystem;
-import de.zib.gndms.infra.configlet.C3MDSConfiglet;
-import de.zib.gndms.kit.network.NetworkAuxiliariesProvider;
-import de.zib.gndms.logic.model.gorfx.AbstractORQCalculator;
-import de.zib.gndms.logic.model.gorfx.c3grid.AbstractProviderStageInORQCalculator;
-import de.zib.gndms.model.gorfx.Contract;
-import de.zib.gndms.model.gorfx.types.GORFXConstantURIs;
-import de.zib.gndms.model.gorfx.types.SliceStageInORQ;
+import de.zib.gndms.GORFX.ORQ.client.ORQClient;
+import de.zib.gndms.GORFX.client.GORFXClient;
 import de.zib.gndms.c3resource.jaxb.Workspace;
-import org.globus.wsrf.container.ServiceHost;
-import org.joda.time.DateTime;
+import de.zib.gndms.infra.configlet.C3MDSConfiglet;
+import de.zib.gndms.logic.model.gorfx.AbstractORQCalculator;
+import de.zib.gndms.model.gorfx.Contract;
+import de.zib.gndms.model.gorfx.types.SliceStageInORQ;
+import de.zib.gndms.typecon.common.type.ContextXSDTypeWriter;
+import de.zib.gndms.typecon.common.type.ContractXSDReader;
+import de.zib.gndms.typecon.common.type.ContractXSDTypeWriter;
+import de.zib.gndms.typecon.common.type.ProviderStageInORQXSDTypeWriter;
 import org.apache.axis.types.URI;
+import types.ContextT;
+import types.OfferExecutionContractT;
+import types.ProviderStageInORQT;
 
 import java.util.Set;
 
@@ -24,74 +27,35 @@ import java.util.Set;
 public class SliceStageInORQCalculator extends
     AbstractORQCalculator<SliceStageInORQ, SliceStageInORQCalculator> {
 
-    // todo find pretty solution for system hack
-    private static GNDMSystem system;
-
 
     public SliceStageInORQCalculator( ) {
         super( SliceStageInORQ.class );
     }
 
 
-    //
-    // offertime = stageing time + transfer-time
     @Override
     public Contract createOffer() throws Exception {
 
-        // create provider staging orq using this this offer type
-        AbstractProviderStageInORQCalculator psi_calc = ( AbstractProviderStageInORQCalculator )
-            getSystem().getInstanceDir().getORQCalculator( getSystem().getEntityManagerFactory(), GORFXConstantURIs.PROVIDER_STAGE_IN_URI );
-        
-        psi_calc.setKey( getKey() );
-        psi_calc.setORQArguments( getORQArguments() );
+        String sid = getORQArguments().getGridSite();
+        getORQArguments().setGridSiteURI( sid );
 
-        Contract c = psi_calc.createOffer();
+        GORFXClient cnt = new GORFXClient( sid );
 
-        if( c.hasExpectedSize() ) {
-            long s = c.getExpectedSize( );
+        ProviderStageInORQT p_orq = ProviderStageInORQXSDTypeWriter.write( getORQArguments() );
+        ContextT ctx = ContextXSDTypeWriter.writeContext( getORQArguments().getContext() );
+        ORQClient orq_cnt = new ORQClient( cnt.createOfferRequest( p_orq, ctx ) );
+        OfferExecutionContractT con = ContractXSDTypeWriter.fromContract( getPerferredOfferExecution() );
+        OfferExecutionContractT con2 = orq_cnt.permitEstimateAndDestroyRequest( con, ctx );
 
-            String src = ServiceHost.getBaseURL( ).getHost();
-            URI dst_uri = destinationURI( getORQArguments().getGridSite() );
-            String dst = dst_uri.getHost( );
-            Float ebw = getNetAux().getBandWidthEstimater().estimateBandWidthFromTo( src, dst );
-
-            if( ebw == null )
-                throw new RuntimeException( "No connection beween" + src +  " and " + dst );
-
-            getORQArguments().setGridSiteURI( dst );
-
-            DateTime dat = new DateTime( c.getDeadline() );
-            dat = dat.plusSeconds( NetworkAuxiliariesProvider.calculateTransferTime( s, ebw ) );
-
-            c.setDeadline( dat.toGregorianCalendar() );
-        }
-
-        return c;
+        return ContractXSDReader.readContract( con2 );
     }
 
 
-    protected static GNDMSystem getSystem( ) {
-        if( system == null )
-            throw new IllegalStateException ( "GNDMS not present" );
-
-        return system;
-    }
-
-    
-    public static void setSystem( GNDMSystem sys ) {
-
-        if( system != null )
-            throw new IllegalStateException ( "GNDMS already present" );
-
-        system = sys;
-    }
-
-
-    public URI destinationURI( String gs ) throws URI.MalformedURIException {
+    public String destinationURI( String gs ) throws URI.MalformedURIException {
         C3MDSConfiglet cfg = getConfigletProvider().getConfiglet( C3MDSConfiglet.class, C3MDSConfiglet.class.getName( ) );
         Set<Workspace.Archive> a = cfg.getCatalog().getArchivesByOid().get( gs );
-        Workspace w = cfg.getCatalog().getWorkspaceByArchive().get( a.toArray()[0] );
-        return new URI( w.getBaseUrl() );
+        
+        return ((Workspace.Archive) a.toArray()[0]).getBaseUrl();
     }
 }
 
