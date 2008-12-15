2ea80bb98d99207742c3324c740afff0cde4e687
mjorra@csr-priv5.zib.de <mjorra@csr-priv5.zib.de>
2008-12-15 16:23:39 +0100

    little changes in sync.sh and added a script to check the CA

--- logic/src/de/zib/gndms/logic/model/TaskAction.java
+++ logic/src/de/zib/gndms/logic/model/TaskAction.java
@@ -325,6 +325,7 @@ public abstract class TaskAction extends AbstractModelAction<AbstractTask, Abstr
 
 
     protected void refreshTaskResource() {
+
         try {
             getPostponedActions().getListener().onModelChange(getModel());
         }
