49abfeb65251f3570ef5d7aba285847d3c8586fc
Maik <mjorra@csr-priv5.zib.de>
2009-02-09 17:16:26 +0100

    Added test class for XML ORQ's and fixed a minor bug in INSTALL.

---
+++ typecon/test-src/de/zib/gndms/typecon/util/FromXMLTest.java
@@ -0,0 +1,49 @@
+package de.zib.gndms.typecon.util;
+
+import de.zib.gndms.model.gorfx.types.io.xml.ORQWrapper;
+import de.zib.gndms.model.gorfx.types.io.ProviderStageInORQPropertyReader;
+import de.zib.gndms.model.gorfx.types.io.ProviderStageInORQStdoutWriter;
+import de.zib.gndms.model.gorfx.types.io.ProviderStageInORQConverter;
+import de.zib.gndms.model.gorfx.types.ProviderStageInORQ;
+import de.zib.gndms.typecon.common.GORFXTools;
+import de.zib.gndms.typecon.common.type.ProviderStageInORQXSDReader;
+import de.zib.gndms.kit.application.AbstractApplication;
+import org.kohsuke.args4j.Option;
+
+import java.io.FileInputStream;
+
+/**
+ * @author Maik Jorra <jorra@zib.de>
+ * @version $Id$
+ *          <p/>
+ *          User: mjorra, Date: 09.02.2009, Time: 16:18:19
+ */
+public class FromXMLTest extends AbstractApplication {
+
+    @Option( name="-fn", required=true, usage="xml request file name" )
+    private String fn;
+
+    public static void main ( String [] args ) throws Exception {
+        FromXMLTest test = new FromXMLTest();
+        test.run( args );
+    }
+
+    public void run () throws Exception {
+
+        FileInputStream ifs = new FileInputStream( fn );
+
+
+        ProviderStageInXMLImpl reader = new ProviderStageInXMLImpl( );
+        ORQWrapper wrp = reader.fromDocument( ifs );
+
+        ifs.close();
+        
+        ProviderStageInORQ orq = (ProviderStageInORQ) wrp.getOrq();
+
+        ProviderStageInORQStdoutWriter stdout = new ProviderStageInORQStdoutWriter();
+        ProviderStageInORQConverter conv = new ProviderStageInORQConverter( stdout, orq );
+
+        conv.convert();
+    }
+
+}
