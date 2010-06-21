554b422a410b7c2b30bffb952b3ba137d18a3eb2
Stefan Plantikow <stefan.plantikow@googlemail.com>
2010-06-21 17:24:26 +0200

    PTGrid specific INSTALL instructions and build scripts

--- scripts/ptgrid/setup-resource.sh
+++ scripts/ptgrid/setup-resource.sh
@@ -3,17 +3,18 @@
 SCRIPTDIR="$(dirname $0)/../" ; source "$SCRIPTDIR"internal/script-setup.sh
 
 STAGING_AREA_PATH="/tmp"
-STAGING_AREA_SIZE="1000000" # Currently unused
 
 # Set your hostname if it is not detected correctly
 # GRIDHOST=""
 
+# URL to GridFTP Space
 STAGING_AREA_GSI_FTP_URL="gsiftp://$GRIDHOST""$STAGING_AREA_PATH"
 
 
 
 # Do not edit below this line unless very sure ------------------------------------------------------------------------
 
+ptgrid_generic
 enable_ptresource
 enable_filetransfer
 enable_interslicetransfer
