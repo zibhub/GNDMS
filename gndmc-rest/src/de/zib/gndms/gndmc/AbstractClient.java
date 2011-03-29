e5d6c3318583837364ab82c2a12c6319f5c3fd31
mjorra <jorra@zib.de>
2011-03-29 12:33:46 +0200

    minor cleanup

--- gndmc-rest/src/de/zib/gndms/gndmc/AbstractClient.java
+++ gndmc-rest/src/de/zib/gndms/gndmc/AbstractClient.java
@@ -121,17 +121,6 @@ public abstract class AbstractClient {
     @Autowired
 	public void setRestTemplate(RestTemplate restTemplate) {
 		this.restTemplate = restTemplate;
-        logger.debug( "resttemplate injected. Provided converters: " );
-        for ( HttpMessageConverter mc : restTemplate.getMessageConverters() ) {
-            logger.debug( mc.getClass().getName() );
-
-            if ( mc.getClass().equals( TestMappingJacksonHttpMessageConverter.class ) ) {
-                TestMappingJacksonHttpMessageConverter conv = TestMappingJacksonHttpMessageConverter.class.cast( mc ) ;
-                ObjectMapper om = conv.getObjectMapper();
-                logger.debug( "om name: " + om.getClass().getName() );
-            } else
-                throw new IllegalStateException( "unexpected converter" );
-        }
 	}
 
 	public String getServiceURL() {
