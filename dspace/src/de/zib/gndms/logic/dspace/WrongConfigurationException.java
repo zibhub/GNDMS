b15ada48a60ea0b513887839733a40d4a11c179b
Ulrike Golas <ulli@golas.zib.de>
2011-05-11 17:23:25 +0200

    some changes of dspace with rest

---
+++ dspace/src/de/zib/gndms/logic/dspace/WrongConfigurationException.java
@@ -0,0 +1,8 @@
+package de.zib.gndms.logic.dspace;
+
+public class WrongConfigurationException extends Exception {
+
+	private static final long serialVersionUID = -6277152854505420912L;
+
+
+}
---
+++ dspace/src/de/zib/gndms/model/dspace/SliceKindConfiguration.java
@@ -0,0 +1,16 @@
+package de.zib.gndms.model.dspace;
+
+import de.zib.gndms.logic.dspace.WrongConfigurationException;
+import de.zib.gndms.stuff.confuror.ConfigHolder;
+
+public class SliceKindConfiguration extends ConfigHolder {
+
+	public SliceKindConfiguration(final ConfigHolder config) throws WrongConfigurationException {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public SliceKindConfiguration(SliceKind sliceK){
+		// TODO there should be a field in Subspace holding its SubspaceConfiguration
+	}
+
+}
