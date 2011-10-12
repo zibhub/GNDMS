cb3edd0fe0b81b279491363a66320c668dc18c6d
mjorra <jorra@zib.de>
2011-10-12 19:02:15 +0200

    merged

--- scripts/c3grid/setup-central-dms-site.sh
+++ scripts/c3grid/setup-central-dms-site.sh
@@ -16,7 +16,6 @@ DMS_AREA_SIZE="10000" # Unused
 
 DMS_AREA_GSI_FTP_URL="gsiftp://$GRIDHOST""$DMS_AREA_PATH"
 
-
 # Do not edit below this line unless very sure ------------------------------------------------------------------------------------------------------------
 
 c3grid_generic
