cc5a280c4caf3e1e7691bdfd96a1ef218eb2368e
Jörg Bachmann <bachmann@zib.de>
2011-11-11 15:34:24 +0100

    Just a new TODO

--- src/main/java/de/zib/adis/Adis.java
+++ src/main/java/de/zib/adis/Adis.java
@@ -14,6 +14,7 @@ import org.slf4j.LoggerFactory;
 
 public class Adis
 {
+        // TODO: static code to check format contract
         protected final Logger log = LoggerFactory.getLogger( this.getClass() );
 
         private RESTClient voldi;
