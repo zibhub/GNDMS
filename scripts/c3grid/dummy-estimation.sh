0560340a3094436a03dbe2e6f8c9e565ce303d21
Jörg Bachmann <bachmann@zib.de>
2012-02-06 19:54:14 +0100

    dummy-estimation uses input & fills with defaults

--- scripts/c3grid/dummy-estimation.sh
+++ scripts/c3grid/dummy-estimation.sh
@@ -11,21 +11,16 @@
 
 #cat - 
 
-cat << EOF
-# Value: ISO formatierte Zeitangabe / long
-#c3grid.StageFileRequest.Estimate.IfDecisionBefore=2006-12-01T00:00:00.000Z
+while read LINE; do
+    [ "${LINE}" != "${LINE#c3grid.StageFileRequest.Estimate.ExecutionLikelyUntil=}" ] && HAVE_LIKELY=true
+    [ "${LINE}" != "${LINE#c3grid.StageFileRequest.Estimate.ResultValidUntil=}" ] && HAVE_VALID=true
+    [ "${LINE}" != "${LINE#c3grid.StageFileRequest.Estimate.MaxSize=}" ] && HAVE_MAXSIZE=true
+    echo "${LINE}"
+done
 
-# Value: ISO formatierte Zeitangabe / long
-# okay for dummy staging
-c3grid.StageFileRequest.Estimate.ExecutionLikelyUntil=5000
- 
-# Value: ISO formatierte Zeitangabe / long
-# keep result for 10 minutes
-c3grid.StageFileRequest.Estimate.ResultValidUntil=600000
- 
-# Value: long
-c3grid.StageFileRequest.Estimate.MaxSize=123456
-EOF
+[ -z "${HAVE_LIKELY}" ] && echo "c3grid.StageFileRequest.Estimate.ExecutionLikelyUntil=5000"
+[ -z "${HAVE_VALID}" ] && echo "c3grid.StageFileRequest.Estimate.ResultValidUntil=600000"
+[ -z "${HAVE_MAXSIZE}" ] && echo "c3grid.StageFileRequest.Estimate.MaxSize=123456"
 
 # exit 255 if unfulfillable
 # exit 254 if permission denied for DN
