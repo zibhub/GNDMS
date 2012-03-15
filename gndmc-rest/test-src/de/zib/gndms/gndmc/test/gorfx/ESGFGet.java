4888ae5cdd412624635d1c4173c59a9479a0858a
Jörg Bachmann <bachmann@zib.de>
2012-03-15 09:55:44 +0100

    Built test for HTTPGetter

--- gndmc-rest/test-src/de/zib/gndms/gndmc/test/gorfx/ESGFGet.java
+++ gndmc-rest/test-src/de/zib/gndms/gndmc/test/gorfx/ESGFGet.java
@@ -16,21 +16,9 @@ package de.zib.gndms.gndmc.test.gorfx;
  */
 
 import de.zib.gndms.common.kit.application.AbstractApplication;
+import de.zib.gndms.gndmc.utils.DownloadResponseExtractor;
 import de.zib.gndms.gndmc.utils.HTTPGetter;
 import org.kohsuke.args4j.Option;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.client.ClientHttpRequest;
-import org.springframework.http.client.ClientHttpResponse;
-import org.springframework.web.client.RequestCallback;
-import org.springframework.web.client.ResponseExtractor;
-import org.springframework.web.client.RestTemplate;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URLDecoder;
-import java.util.LinkedList;
-import java.util.List;
 
 /**
  * @author Maik Jorra
@@ -71,80 +59,9 @@ public class ESGFGet extends AbstractApplication {
         getter.setPassword( passwd );
         getter.setupSSL();
 
-        MyResponseExtractor responseExtractor = get( url, null );
-        int statusCode = responseExtractor.getStatusCode();
-
-        while( 302 == statusCode) {
-            final List< String > cookie = responseExtractor.getCookie();
-            final String location = responseExtractor.getLocation();
-
-            responseExtractor = get( location, new RequestCallback() {
-                @Override
-                public void doWithRequest( ClientHttpRequest request ) throws IOException {
-                    for( String c: cookie )
-                        request.getHeaders().add("Cookie", c.split(";", 2)[0]);
-                }
-            });
-
-            statusCode = responseExtractor.getStatusCode();
-        }
-    }
-    
-    
-    MyResponseExtractor get( String url, RequestCallback requestCallback ) {
-        MyResponseExtractor responseExtractor = new MyResponseExtractor();
-
-        RestTemplate rt = new RestTemplate();
-        rt.execute(url, HttpMethod.GET, requestCallback, responseExtractor);
-
-        return responseExtractor;
-    }
-    
-    
-    class MyResponseExtractor implements ResponseExtractor< Object > {
+        getter.setExtractor( 200, new DownloadResponseExtractor() );
+        int statusCode = getter.get( url );
         
-        private int statusCode;
-        private List< String > cookie = null;
-        private String location = null;
-        private HttpHeaders headers;
-        private InputStream inputStream;
-
-        @Override
-        public Object extractData( final ClientHttpResponse response ) throws IOException {
-            
-            statusCode = response.getStatusCode().value();
-            headers = response.getHeaders();
-            
-            for( String header: headers.keySet() ) {
-                if( "Set-Cookie".equals( header ) ) {
-                    cookie = new LinkedList< String >();
-
-                    for( String value: headers.get( header ) ) {
-                        cookie.add( value );
-                    }
-                }
-                else if( "Location".equals( header ) ) {
-                    location = URLDecoder.decode( headers.get( header ).get( 0 ) );
-                }
-            }
-
-            return null;
-        }
-
-        public int getStatusCode() {
-            return statusCode;
-        }
-
-        public List< String > getCookie() {
-            return cookie;
-        }
-
-        public String getLocation() {
-            return location;
-        }
-
-        public HttpHeaders getHeaders() {
-            return headers;
-        }
+        System.out.println( "StatusCode: " + statusCode );
     }
 }
