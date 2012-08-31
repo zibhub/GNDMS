4b5455f09693f0a94291bd85b3d7fde1df79a66a
Jörg Bachmann <bachmann@zib.de>
2012-08-31 14:31:32 +0200

    Switched back to JodaTime-1.6.2 because of gndms-buildr

--- common/src/de/zib/gndms/common/model/gorfx/types/io/SliceOrderPropertyReader.java
+++ common/src/de/zib/gndms/common/model/gorfx/types/io/SliceOrderPropertyReader.java
@@ -18,7 +18,7 @@ package de.zib.gndms.common.model.gorfx.types.io;
 
 import de.zib.gndms.common.dspace.SliceConfiguration;
 import de.zib.gndms.common.model.gorfx.types.SliceOrder;
-import org.joda.time.DateTime;
+import org.joda.time.format.ISODateTimeFormat;
 
 import java.util.Properties;
 
@@ -51,12 +51,14 @@ public abstract class SliceOrderPropertyReader< M extends SliceOrder > extends O
         final String sliceId = properties.getProperty( SliceOrderProperties.SLICE_ID.key );
         final String sliceSize = properties.getProperty( SliceOrderProperties.SLICE_SIZE.key );
         final String sliceTermTime = properties.getProperty( SliceOrderProperties.SLICE_TERMINATION_TIME.key );
-
+        
         if( sliceId != null )
             sliceOrder.setSliceId( sliceId );
         else if( sliceSize != null || sliceTermTime != null ) {
             sliceOrder.setSliceConfiguration(
-                    new SliceConfiguration( Long.parseLong( sliceSize ), DateTime.parse( sliceTermTime ) )
+                    new SliceConfiguration(
+                            Long.parseLong( sliceSize ),
+                            ISODateTimeFormat.dateTimeParser().parseDateTime(sliceTermTime) )
             );
         }
     }
