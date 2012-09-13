b7e073b7fab5ee1e9eeb25580894cdd488bfb65e
Jörg Bachmann <bachmann@zib.de>
2012-09-13 11:14:38 +0200

    Adjusted run-test-client script for FileTransfer

--- taskflows/fileTransfer/bin/run-transfer-client.sh
+++ taskflows/fileTransfer/bin/run-transfer-client.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-VERSION="0.6.2"
+VERSION="0.6.3"
 args=( "-uri" "https://${GNDMS_HOST:-$(hostname -f)}:${GNDMS_PORT:-8443}/gndms/c3grid/" )
 
 cp="$HOME/.m2/repository/commons-codec/commons-codec/1.4/commons-codec-1.4.jar:$cp"
 cp="$HOME/.m2/repository/org/slf4j/jcl-over-slf4j/1.6.3/jcl-over-slf4j-1.6.3.jar:$cp"
 cp="$GNDMS_SOURCE/lib/stuff/gndms-stuff-$VERSION.jar:$cp"
 cp="$GNDMS_SOURCE/lib/common/gndms-common-$VERSION.jar:$cp"
-cp="$GNDMS_SOURCE/lib/gndmc-rest/gndms-gndmc-rest-0.6.1.jar:$cp"
+cp="$GNDMS_SOURCE/lib/gndmc-rest/gndms-gndmc-rest-0.6.3.jar:$cp"
 cp="$HOME/.m2/repository/com/intellij/annotations/7.0.3/annotations-7.0.3.jar:$cp"
-cp="$HOME/.m2/repository/joda-time/joda-time/1.6/joda-time-1.6.jar:$cp"
+cp="$HOME/.m2/repository/joda-time/joda-time/1.6.2/joda-time-1.6.2.jar:$cp"
 cp="$HOME/.m2/repository/args4j/args4j/2.0.14/args4j-2.0.14.jar:$cp"
 cp="$HOME/.m2/repository/javax/inject/javax.inject/1/javax.inject-1.jar:$cp"
 cp="$HOME/.m2/repository/commons-lang/commons-lang/2.1/commons-lang-2.1.jar:$cp"
@@ -28,7 +28,7 @@ cp="$HOME/.m2/repository/org/springframework/spring-core/3.1.0.RELEASE/spring-co
 cp="$HOME/.m2/repository/org/springframework/spring-expression/3.1.0.RELEASE/spring-expression-3.1.0.RELEASE.jar:$cp"
 cp="$HOME/.m2/repository/org/springframework/spring-oxm/3.1.0.RELEASE/spring-oxm-3.1.0.RELEASE.jar:$cp"
 cp="$HOME/.m2/repository/org/springframework/spring-web/3.1.0.RELEASE/spring-web-3.1.0.RELEASE.jar:$cp"
-cp="$HOME/.m2/repository/com/thoughtworks/xstream/xstream/1.3.1/xstream-1.3.1.jar:$cp"
+cp="$HOME/.m2/repository/com/thoughtworks/xstream/xstream/1.4.1/xstream-1.4.1.jar:$cp"
 cp="$HOME/.m2/repository/cglib/cglib-nodep/2.2/cglib-nodep-2.2.jar:$cp"
 cp="$HOME/.m2/repository/dom4j/dom4j/1.6.1/dom4j-1.6.1.jar:$cp"
 cp="$HOME/.m2/repository/org/codehaus/jettison/jettison/1.0.1/jettison-1.0.1.jar:$cp"
